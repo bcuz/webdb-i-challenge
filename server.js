bcd16af29c79904ac4f8bccade6472d3094229d4
adam <bcuz@users.noreply.github.com>
2019-05-22 10:02:23 -0400

    get account by id

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const Accounts = require('./data/accounts-model.js');
 
 const server = express();
 
-// your code here
+server.get('/accounts/:id', async (req, res) => {
+
+  try {
+    const account = await Accounts.findById(req.params.id);      
+
+    if (account) {
+      res.status(200).json(account);
+    } else {
+      res.status(404).json({ message: 'account not found' });
+    }
+  } catch (error) {
+    // log error to server
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the account',
+    });
+  }  
+});
 
 module.exports = server;
