e4acfd578aa570366bb5afbed6b1129bae830bc8
adam <bcuz@users.noreply.github.com>
2019-05-22 21:35:10 -0400

    create an account

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const Accounts = require('./data/accounts-model.js');
 
 const server = express();
+server.use(express.json());
+
 
 server.get('/accounts/:id', async (req, res) => {
-
+  // might refactor later
   try {
     const account = await Accounts.findById(req.params.id);      
 
@@ -20,6 +22,27 @@ server.get('/accounts/:id', async (req, res) => {
       message: 'Error retrieving the account',
     });
   }  
+});
+
+server.post('/accounts', async (req, res) => {
+  // no specific error for non-unique name
+  let { name, budget } = req.body;
+
+  if (!name || !budget) {    
+    return res.status(400).json({ message: "Please provide name and budget for the account." });
+  }
+
+  try {
+    const account = await Accounts.add(req.body);
+    res.status(201).json(account);
+  } catch (error) {
+    // log error to server
+    console.log(error);
+    res.status(500).json({
+      message: 'Error adding the account',
+    });
+  }
+  
 });
 
 module.exports = server;
